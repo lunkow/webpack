515f6ed9774506a4f5f979d48ad489300a8c17db
Anton Lunkov <lunkow@live.ru>
2022-04-10 19:44:40 +0300

    Code splitting: prevent duplication (split chunks plugin)

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -22,7 +22,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var loda
 },
 /******/ __webpack_require__ => { // webpackRuntimeModules
 /******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ __webpack_require__.O(0, ["shared"], () => (__webpack_exec__("./src/index.js")));
-/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
 /******/ }
 ]);
